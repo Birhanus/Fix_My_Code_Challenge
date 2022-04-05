e07b66bf4e834b8f68837a684fb29e3e014238f5
birhanus <birhanushimles716@gmail.com>
2022-04-05 21:25:41 +0000

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,6 +10,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Square class initialized """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,15 +19,17 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return self.width + self.width + self.height + self.height
 
     def __str__(self):
+        """ String representation of the square instance """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Create Square instance """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
